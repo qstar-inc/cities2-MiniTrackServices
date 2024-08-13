56f0f958aec825000c61ae5f9ab85b7d6300b41b
Qoushik <qstar.inc@gmail.com>
2024-08-13 21:28:17 +0600

    Add project files.

---
+++ Mod.cs
@@ -0,0 +1,30 @@
+﻿using Colossal.Logging;
+using Colossal.UI;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+using System.IO;
+
+namespace MiniTrackServices
+{
+    public class Mod : IMod
+    {
+        public static string Name = "Mini Track Services";
+        public static string Version = "1.0.0";
+        public static string Author = "StarQ";
+        //public static ILog log = LogManager.GetLogger($"{nameof(MiniTrackServices)}").SetShowsErrorsInUI(false);
+
+        public static string uiHostName = "starq-minitrackservices";
+
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset);
+            UIManager.defaultUISystem.AddHostLocation(uiHostName, Path.Combine(Path.GetDirectoryName(asset.path), "thumbs"), false);
+        }
+        
+        public void OnDispose()
+        {
+            UIManager.defaultUISystem.RemoveHostLocation(uiHostName);
+        }
+    }
+}
